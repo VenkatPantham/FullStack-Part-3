5da53cc20a0b4dde7f811bd613346ec72560b790
VenkatPantham <venkatpantham5568@gmail.com>
2020-05-20 16:34:02 +0530

    Using Redux

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -12,6 +12,24 @@ import About from "./AboutComponent";
 import Menu from "./MenuComponent";
 import Dishdetail from "./DishdetailComponent";
 import Contact from "./ContactComponent";
+import { connect } from "react-redux";
+import {
+  fetchDishes,
+  fetchComments,
+  fetchPromos,
+  fetchLeaders,
+} from "../redux/ActionCreators";
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchDishes: () => dispatch(fetchDishes()),
+  fetchComments: () => dispatch(fetchComments()),
+  fetchPromos: () => dispatch(fetchPromos()),
+  fetchLeaders: () => dispatch(fetchLeaders()),
+});
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -178,6 +196,13 @@ const MainNavigatorDrawer = () => {
 };
 
 class Main extends Component {
+  componentDidMount() {
+    this.props.fetchDishes();
+    this.props.fetchComments();
+    this.props.fetchPromos();
+    this.props.fetchLeaders();
+  }
+
   render() {
     return (
       <NavigationContainer>
@@ -211,4 +236,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Main;
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
